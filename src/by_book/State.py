5460b3bc7dafe61da3939e143566068d4071f73a
Emil Terman <terman.emil@gmail.com>
2024-03-10 18:03:34 +0100

    Fix an import error

--- src/by_book/State.py
+++ src/by_book/State.py
@@ -2,7 +2,6 @@ from dataclasses import dataclass
 
 import numpy as np
 
-from State import State
 from by_book.Board import Board
 
 
@@ -14,5 +13,5 @@ class State:
     def __str__(self):
         return f'Player to move: {self.player_to_move}\n{self.board}'
 
-    def __eq__(self, other: State):
+    def __eq__(self, other: 'State'):
         return self.player_to_move == other.player_to_move and np.array_equal(self.board.matrix, other.board.matrix)
