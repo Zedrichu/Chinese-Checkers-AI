418e0aedb8fc31236ebff4c61b7a059a22e64087
Zedrichu <adrzvizdencojr@gmail.com>
2024-03-10 13:13:48 +0100

    Fixed some conflicts in Jeppe's branch

---
+++ src/GameController.py
@@ -0,0 +1,15 @@
+from src.player import Player
+import pygame
+from src.GameProblem import Step
+
+
+# Class to control the game
+# TODO: Refactor the code to make it more readable, proof of concept
+class GameController:
+    def __init__(self, board):
+        self.turn = 1
+        self.board = board
+        # self.players = [Player(i) for i in range(1,3)]
+
+    def end_turn(self):
+        self.turn = self.turn % 2 + 1
