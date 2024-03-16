6894dbe27d609b4be56f05de64c3c8ddd85412e6
Emil Terman <terman.emil@gmail.com>
2024-03-16 13:23:42 +0100

    Fix issue with pop-ing an element from a set
    poping an element from a set removes a random element from it, therefore an additional data structure was introduced - a qeue, so that we can keep track of the oldest element in the list

--- tests/test_state_hashing.py
+++ tests/test_state_hashing.py
@@ -17,3 +17,11 @@ class TestStateHashing(unittest.TestCase):
         state2 = sut.initial_state()
 
         self.assertEqual(hash(state1), hash(state2))
+
+    def test_hashing_two_different_states(self):
+        sut = ChineseCheckers(triangle_size=2)
+        state1 = sut.initial_state()
+        state2 = sut.initial_state()
+        state2.board.move((3, 0), (2, 0))
+
+        self.assertNotEqual(hash(state1), hash(state2))
