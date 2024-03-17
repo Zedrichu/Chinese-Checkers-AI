dedada8cefd9512c042abb98fa8c0dcca0e81361
kubekj <kuba.jedraszka@gmail.com>
2024-03-17 17:42:07 +0100

    Fixed main

--- main.py
+++ main.py
@@ -1,32 +1,23 @@
 import argparse
 import sys
 
-from benchmarking.GameAnalytics import GameAnalytics
-
 sys.path.append("src")
 from GameController import GameController
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Chinese Checkers game with AI and player options.')
 
-    if True:
-        analytics = GameAnalytics()
-        analytics.load_from_file('game_data.json')
-        analytics.plot()
-    else:
+    parser.add_argument('--first-player', choices=['human', 'minimax'], required=False,
+                        help='Type of the first player.')
+    parser.add_argument('--first-minimax-depth', type=int, default=6, required=False,
+                        help='Minimax depth for the first player, if applicable.')
+    parser.add_argument('--second-player', choices=['random', 'nonrepeatrandom', 'minimax'], required=False,
+                        help='Type of the second player.')
+    parser.add_argument('--second-minimax-depth', type=int, default=6, required=False,
+                        help='Minimax depth for the second player, if applicable.')
 
-        parser = argparse.ArgumentParser(description='Chinese Checkers game with AI and player options.')
+    args = parser.parse_args()
 
-        parser.add_argument('--first-player', choices=['human', 'minimax'], required=False,
-                            help='Type of the first player.')
-        parser.add_argument('--first-minimax-depth', type=int, default=6, required=False,
-                            help='Minimax depth for the first player, if applicable.')
-        parser.add_argument('--second-player', choices=['random', 'nonrepeatrandom', 'minimax'], required=False,
-                            help='Type of the second player.')
-        parser.add_argument('--second-minimax-depth', type=int, default=6, required=False,
-                            help='Minimax depth for the second player, if applicable.')
+    controller = GameController(verbose=False, use_graphics=False, args=args)
 
-        args = parser.parse_args()
-
-        controller = GameController(verbose=False, use_graphics=False, args=args)
-
-        controller.game_loop()
+    controller.game_loop()
